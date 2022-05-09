9c3986f88b5cf098acc78b56208c347d47a91cb0
LashaKutsia <lasha.kutsia.1@btu.edu.ge>
2022-05-09 17:19:53 +0100

    first

---
+++ main.py
@@ -0,0 +1,16 @@
+import os
+os.system("pip install requests")
+import json
+
+import requests
+key = '0484d2f1ceae50a691c68634ab21f46b'
+city = input("დაასახელეთ ქალაქი: ")
+resp = requests.get(f'http://api.openweathermap.org/geo/1.0/direct?q={city}&appid={key}')
+print(resp.text)
+print(resp.status_code)
+print(resp.headers)
+r = json.loads(resp.text)
+with open('data.name', 'w')as file:
+    json.dump(r, file, indent=4)
+
+
